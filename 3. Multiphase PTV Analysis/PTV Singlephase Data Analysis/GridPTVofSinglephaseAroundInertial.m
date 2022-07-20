69cdba61bb81e2b7518e37f3684e9d5329921aff
Miguel X. Diaz-Lopez <mdialo1@jhu.edu>
2022-07-20 11:29:33 -0400

    Finalizing PTV_Analysis_OfSinglephase and adding GridPTVofSinglephaseAroundInertial

---
+++ 3. Multiphase PTV Analysis/PTV Singlephase Data Analysis/GridPTVofSinglephaseAroundInertial.m
@@ -0,0 +1,65 @@
+%% Directories
+Tnum = 3;
+datdirec = ['E:\PIV Data\Raw Data\2022_06_30\T' num2str(Tnum)];
+processeddirec = ['E:\PIV Data\Processed Data\2022_06_30\T' num2str(Tnum)];
+analyzeddirec = ['E:\PIV Data\Analyzed Results\2022_06_30\T' num2str(Tnum)];
+
+% Plot settings
+axiswidth = 2; linewidth = 2; fontsize = 18;
+red_color = '#de2d26'; blue_color = '#756bb1';
+green_color = '#31a354'; black_color = '#000000';
+
+ParticleDiameter = 200e-6;
+dperPix = 6.625277859765377e-06;
+
+
+%% Load necessary data and obtain Run and Frame numbers
+
+load([analyzeddirec '\LPTData.mat'],'vtracks','tracks')
+load([analyzeddirec '\PIVData.mat'])
+load([analyzeddirec '\InertialParticalSelection.mat'], 'tracksParticleIndex')
+
+%% Setting up settings for interrogation window
+
+IntWinSize = 4; %pixels %Getting the Interrogation window size of the PIV data
+D_HL = 12;%Number of interrogation windows to the left
+D_HR = 24; %Number of interrogation windows to the right
+D_VUP = 8; %Number of inerrogation windows above the particle
+D_VD = 8; %Number of interrogation
+% Diameter = ceil(ParticleDiameter/dperPix) + 10;
+Diameter = 60;
+imageSizeX = 450; imageSizeY = 250;
+[columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
+
+% Particle Information
+for Run = 1:numel(tracksParticleIndex)
+     disp(['On Run ' num2str(Run) ' of ' num2str(numel(tracksParticleIndex))])
+    for m = 1:numel(tracksParticleIndex{Run})
+
+
+        ParticleNum = tracksParticleIndex{Run}(m);
+
+        ParticleLocationX = tracks{Run}(ParticleNum).X; %Pix
+        ParticleLocationY = tracks{Run}(ParticleNum).Y; %Pix
+        ParticleVelocityU = mean(vtracks{Run}(ParticleNum).U)*dperPix*FPS; %m/s
+        ParticleVelocityV = mean(vtracks{Run}(ParticleNum).V)*dperPix*FPS; %m/s
+
+        % Setting up bounds for this particle
+        LeftBound = ceil(ParticleLocationX - Diameter/2 - D_HL*IntWinSize);
+        RightBound = ceil(ParticleLocationX + Diameter/2+D_HR*IntWinSize);
+        UpperBound = ceil(ParticleLocationY + Diameter/2+D_VUP*IntWinSize);
+        LowerBound = ceil(ParticleLocationY - Diameter/2 - D_VD*IntWinSize);
+% 
+%         for Frame = 1:numel(ParticleLocationX)
+
+            circlePixels = (rowsInImage - ParticleLocationY(Frame)).^2 + (columnsInImage - ParticleLocationX(Frame)).^2 <= (Diameter/2).^2; %Creating logical array size of final image with 1's where the particle is and 0's everywhere else
+
+            [xgrid,ygrid] = meshgrid(LeftBound:IntWinSize:RightBound, LowerBound:IntWinSize:UpperBound);
+            SumUInertial = zeros(size(xgrid,1),size(xgrid,2));
+            SumVInertial = zeros(size(xgrid,1),size(xgrid,2));
+            Iterations = zeros(size(xgrid,1),size(xgrid,2));
+            WeightScale = zeros(size(xgrid,1),size(xgrid,2));
+
+
+    end
+end
--- 3. Multiphase PTV Analysis/PTV Singlephase Data Analysis/PTV_Analysis_OfSinglephase.m
+++ 3. Multiphase PTV Analysis/PTV Singlephase Data Analysis/PTV_Analysis_OfSinglephase.m
@@ -1,24 +1,69 @@
-%% Setting up directories
-Tnum = 5;
-datdirec = ['E:\PIV Data\Raw Data\2022_07_01\T' num2str(Tnum)];
-processeddirec = ['E:\PIV Data\Processed Data\2022_07_01\T' num2str(Tnum)];
-analyzeddirec = ['E:\PIV Data\Analyzed Results\2022_07_01\T' num2str(Tnum)];
+%% Directories
+Tnum = 3;
+datdirec = ['E:\PIV Data\Raw Data\2022_06_30\T' num2str(Tnum)];
+processeddirec = ['E:\PIV Data\Processed Data\2022_06_30\T' num2str(Tnum)];
+analyzeddirec = ['E:\PIV Data\Analyzed Results\2022_06_30\T' num2str(Tnum)];
 
 % Plot settings
 axiswidth = 2; linewidth = 2; 
 red_color = '#de2d26'; blue_color = '#756bb1';
 green_color = '#31a354'; black_color = '#000000';
 
+sharpen = 1;
+
+%% Sharpen image to individual particles are easier to see
+if sharpen == 1
+    A = dir([processeddirec '\Tracer Particles\R*']); ProcessedRuns = {};
+    [ProcessedRuns{1:length(A),1}] = A.name;
+    ProcessedRuns = sortrows(ProcessedRuns)'; NumOfRuns = numel(ProcessedRuns); clear A
+    for k = 1:NumOfRuns
+        inputnames = [processeddirec '\Tracer Particles' '\R' num2str(k)];
+        A = dir([inputnames '\*.tiff']); Image = {};
+        [Image{1:length(A),1}] = A.name;
+        Images = sortrows(Image)'; clear A
+
+        for i = 1:numel(Images)
+            temp = imread([inputnames '\' Images{i}]);
+
+            tempSharpen = imsharpen(temp,'Amount',1.2,'Radius',1,'Threshold',0);
+
+            if ~exist([processeddirec '\Sharpened Tracer Particles\R' num2str(k)],'dir')
+                mkdir([processeddirec '\Sharpened Tracer Particles\R' num2str(k)])
+            end
+        
+
+            imwrite(tempSharpen,[processeddirec '\Sharpened Tracer Particles\R' num2str(k) filesep Images{i}])
+        end
+    end
+    
+end
+
 %% Analysis
+
+Threshold = 50000;
+area_lim = 1;
+dperPix = 6.625277859765377e-06;
+max_disp = 12;
 
 addpath('.\Oulette codes\')
 suffix = '*.tiff';
-A = dir([processeddirec '\Tracer Particles\R*']); ProcessedRuns = {};
+A = dir([processeddirec '\Sharpened Tracer Particles\R*']); ProcessedRuns = {};
     [ProcessedRuns{1:length(A),1}] = A.name;
     ProcessedRuns = sortrows(ProcessedRuns)'; NumOfRuns = numel(ProcessedRuns); clear A
-
+m = 0;
 for k = 1:NumOfRuns
-    disp(['Run ' num2str(k) ' of ' num2str(NumOfRuns)])
-    [x{k},y{k},u{k},v{k},u_filt{k},v_filt{k}] = Miguel_PIVlab([processeddirec '\Tracer Particles' '\R' num2str(k) ], suffix,Mask);
-clc;
+%     disp(['Run ' num2str(k) ' of ' num2str(NumOfRuns)])
+    inputnames = [processeddirec '\Sharpened Tracer Particles' '\R' num2str(k) '\*.tiff'];
+    A = dir(inputnames); ImageNum = {};
+    [ImageNum{1:length(A),1}] = A.name;
+    ImageNum = sortrows(ImageNum)'; clear A
+
+    if numel(ImageNum)>=7
+        m = m+1;
+        [vtracks{m},ntracks{m},meanlength{m},rmslength{m},tracks{m}] = PredictiveTracker(inputnames,Threshold,max_disp,[],area_lim,0,0);
+    end
+% clc;
 end
+
+
+save([analyzeddirec '\PTV_Singlephase'],'vtracks','tracks')
